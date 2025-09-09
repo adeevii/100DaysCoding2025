0c50870032cbd5ce60910b212132cba9daa6f7de
Ade Evi <adevyskz31@gmail.com>
2025-09-09 19:00:36 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,14 @@
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.print(" Hai");
+        System.out.print(" Semua\n");
+
+        System.out.println(" selamat malam");
+
+        String a = " dari saya";
+        String b = " ade";
+
+        System.out.printf("%s %s%n", a, b);
+    }
+}
+
