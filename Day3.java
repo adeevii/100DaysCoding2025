c8567a4d3377900c616ff55549e153f76461fc87
Ade Evi <adevyskz31@gmail.com>
2025-09-10 18:58:45 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.print(" Hai"); //ini untuk mencetak "hai" tanpa membuat baris baru
+        System.out.print(" Semua\n"); //untuk mencetak "semua" 
+
+        System.out.println(" selamat malam"); //mencetak "selamat malam" dalam baris baru
+
+        String a = " dari saya"; //membuat variabel dengan tipe data String
+        String b = " ade";
+
+        System.out.printf("%s %s%n", a, b); //mencetak string
+    }
+}
