c118eb2f47b21f70ace078ff9ece1a2583e15ac0
Ade Evi <adevyskz31@gmail.com>
2025-09-11 18:05:01 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,10 @@
+public class Day4{
+  public static void main(String[] args) {
+    System.out.println("Hai \nYiran");
+
+    System.out.println("Nama:\tYiran");
+    System.out.println("Umur:\t25");
+
+    System.out.println("Yiran say: \"Hai juga\"");
+  }
+}
