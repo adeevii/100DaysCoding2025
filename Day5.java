43667a6cc9598f053822195b147d5df85a783932
Ade Evi <adevyskz31@gmail.com>
2025-09-12 16:47:55 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5 {
+  public static void main(String[] args) {
+    byte a = 100;
+    System.out.println("Nilai byte : " + a);
+
+    short b = 1000;
+    System.out.println("Nilai short : " + b);
+
+    int c = 1000000;
+    System.out.println("Nilai int : " + c);
+
+    long d = 10000000000L;
+    System.out.println("Nilai long : " + d);
+  }
+}
+    
