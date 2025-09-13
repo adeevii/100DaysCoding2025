9015460e7175eb90bba1bb5ed817db9cc40e10c1
Ade Evi <adevyskz31@gmail.com>
2025-09-13 18:31:22 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+   public static void main(String[] args) {
+     float a = 1.2f;
+     double b = 150.3;
+
+     System.out.println("Nilai float : " + a);
+     System.out.println("Nilai double : " + b);
+   }
+}
+ 
