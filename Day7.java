61745402ac2bf02225182bcd7bd74bfd60e31980
Ade Evi <adevyskz31@gmail.com>
2025-09-14 18:40:03 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Day7 {
+   public static void main(String[] args) {
+     char kelas = 'E';
+     boolean status = true; //hanya bernilai true dan false
+
+     System.out.println(kelas);
+     System.out.println(status);
+   }
+}
+  
