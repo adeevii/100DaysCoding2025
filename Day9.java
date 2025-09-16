0db7d75c28647aa1eeccd4752e9def897a1a8b42
Ade Evi <adevyskz31@gmail.com>
2025-09-16 19:14:28 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,15 @@
+public class Day9 {
+ public static void main(String[] args) {
+    //deklarasi variabel
+   String nama;
+
+   //inisialisasi variabel
+   nama = "Yiran";
+   System.out.println("Nama awal : " + nama);
+
+   //update variabel
+   nama = "Jaemin";
+   System.out.println("Nama setelah update : " + nama);
+ }
+}
+  
