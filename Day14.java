3efb20449a3be149b444e7a51b1a088750f38aea
Ade Evi <adevyskz31@gmail.com>
2025-09-21 19:12:34 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,7 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int a = 1223;
+        String b = Integer.toString(a); //ubah ke String
+        System.out.println("Hasil String : " + b);
+    }
+}
