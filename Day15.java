19c358f496ec69b5c29b78bd80f71e34246a2082
Ade Evi <adevyskz31@gmail.com>
2025-09-22 19:30:15 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,10 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 8;
+
+        int hasil = a + b - c;
+        System.out.println("Hasil : " + hasil);
+    }
+}
