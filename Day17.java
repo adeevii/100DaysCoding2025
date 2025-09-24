9509108314111a987f1e03c109a4cdac1054b2a2
Ade Evi <adevyskz31@gmail.com>
2025-09-24 20:09:00 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,9 @@
+public class Day17 {
+    public static void main(String[] args){
+       int a = 25;
+       int b = 5;
+
+       int hasil = a % b;
+       System.out.println("Hasil : " + hasil);
+    }
+}
