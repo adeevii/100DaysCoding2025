3d3f13dc9ad48c8d9d7a68c1954eb37a89d73579
Ade Evi <adevyskz31@gmail.com>
2025-09-25 19:54:51 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,9 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int angkaInt = 1;
+        double angkaDouble = angkaInt;
+
+        System.out.println("int   : " + angkaInt);
+        System.out.println("Double : " + angkaDouble);
+    }
+}
