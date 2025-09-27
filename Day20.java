b9fda9b8faf6c661714ca5ddba7c29ef21f429b8
Ade Evi <adevyskz31@gmail.com>
2025-09-27 20:25:15 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 10.0;
+
+        String strInt = String.valueOf(a);
+        String strDoub = String.valueOf(b);
+        System.out.println("Int ke String : " + strInt);
+        System.out.println("Double ke String : " + strDoub);
+    }
+}
