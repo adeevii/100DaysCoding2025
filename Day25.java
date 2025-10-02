38fd984cfa43c07b60a503cbe5fc5ab67769efa9
Ade Evi <adevyskz31@gmail.com>
2025-10-02 19:15:33 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Day25 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        //increment
+        a++;
+        System.out.println("Setelah increment : " +a);
+
+        //decrement
+        a--;
+        System.out.println("Setelah decrement : " +a);
+    }
+}
