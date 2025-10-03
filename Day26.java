553007651f9d54c33b30a0461b6dd65be7dcbe73
Ade Evi <adevyskz31@gmail.com>
2025-10-03 18:41:40 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,21 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Nilai awal : " +a);
+
+        a += 5;
+        System.out.println("After += : " +a);
+
+        a -= 4;
+        System.out.println("After -= : " +a);
+
+        a *= 3;
+        System.out.println("After *= : " +a);
+
+        a /= 3;
+        System.out.println("After /= : " +a);
+
+        a %= 2;
+        System.out.println("After %= : " +a);
+    }
+}
