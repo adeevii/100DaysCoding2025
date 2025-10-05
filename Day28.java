2b263bcb7a86a3123eb24c4f7a986b46a5bb0cd5
Ade Evi <adevyskz31@gmail.com>
2025-10-05 20:32:55 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Nilai a : "+a);
+        System.out.println("Nilai b : "+b);
+
+        boolean hasil = (a == b);
+        System.out.println("Apa a sama dengan b? : "+ hasil);
+        boolean hasil2 = (a != b);
+        System.out.println("Apa a tidak sama dengan b? : "+hasil2);
+
+    }
+}
