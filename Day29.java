9266b7816b716c711477b8d739f94dc8b4f306de
Ade Evi <adevyskz31@gmail.com>
2025-10-06 22:13:17 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,15 @@
+public class Day29 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Nilai a : "+a);
+        System.out.println("Nilai b : "+b);
+
+        boolean lebihBesar = (a > b);
+        System.out.println("Apa a lebih besar dari b? : "+lebihBesar);
+
+        boolean lebihKecil = (a < b);
+        System.out.println("Apa a lebih kecil dari b? : "+lebihKecil);
+    }
+}
