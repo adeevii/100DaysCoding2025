1b3204a9fd364a66f403a269fe9a4c04aa1a862d
Ade Evi <adevyskz31@gmail.com>
2025-10-08 21:09:52 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,11 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int nilai = 85;
+        int absensi = 90;
+
+        System.out.println("Nilai siswa : "+nilai);
+        System.out.println("Presentase kehadiran : "+absensi+"%");
+        boolean hasil = (nilai >= 75) && (absensi >= 80);
+        System.out.println("Apakah siswa lulus? : "+hasil);
+    }
+}
