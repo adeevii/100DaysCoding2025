2c8be8cac7bda46102eb79373b440c39680ae1f7
Ade Evi <adevyskz31@gmail.com>
2025-10-10 21:59:00 +0800

    Operator logika NOT

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class Day33 {
+    public static void main(String[] args) {
+        boolean masuk = false;
+        System.out.println("Apakah Dosennya Masuk? "+masuk);
+
+        boolean tdkMasuk = !masuk;
+        System.out.println("Apakah Dosennya Tidak Masuk? "+tdkMasuk);
+    }
+}
