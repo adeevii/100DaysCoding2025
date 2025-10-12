f3dd61eedaaa0d69bc4bf8d1920ba7f5b75d4328
Ade Evi <adevyskz31@gmail.com>
2025-10-12 19:01:29 +0800

    percabangan if

---
+++ Day35.java
@@ -0,0 +1,9 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int nilai = 85;
+
+        if (nilai >= 75) {
+            System.out.println("Lulus");
+        }
+    }
+}
