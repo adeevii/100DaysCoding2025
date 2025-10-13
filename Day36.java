335ea67f9689f06039bd97cb85e14e61c4e88ea5
Ade Evi <adevyskz31@gmail.com>
2025-10-13 20:56:04 +0800

    nasted if

---
+++ Day36.java
@@ -0,0 +1,11 @@
+public class Day36 {
+    public static void main(String[] args) {
+        int nilai = 65;
+
+        if (nilai >= 75) {
+            if (nilai >= 65) {
+                System.out.println("Nilai sangat baik");
+            }
+        }
+    }
+}
