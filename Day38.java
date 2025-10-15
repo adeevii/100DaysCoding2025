d5ae5f98d7fc6cffd28736066de673925ab5f14c
Ade Evi <adevyskz31@gmail.com>
2025-10-15 20:29:05 +0800

    menentukan bilangan positif, negatif dan nol

---
+++ Day38.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+
+        int angka = sc.nextInt();
+
+        if (angka > 0) {
+            System.out.println("Bilangan positif");
+        } else if (angka < 0) {
+            System.out.println("Bilangan negatif");
+        } else {
+            System.out.println("Bilangan nol");
+        }
+    }
+}
