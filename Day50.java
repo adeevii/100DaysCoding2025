a7c1d0c01c1a49ba50b6724b24dda7dd84d0bcbc
Ade Evi <adevyskz31@gmail.com>
2025-10-28 00:06:59 +0800

    Add Day50.java to check even or odd numbers

---
+++ Day50.java
@@ -0,0 +1,9 @@
+public class Day50 {
+    public static void main(String[] args) {
+        System.out.print("Masukan angka : ");
+        int a = 2;
+
+        String b = (a % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println("Bilangan "+b);
+    }
+}
