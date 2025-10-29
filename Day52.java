c36f24faed77b1ef1c58e5ce5161684cb50833a4
Ade Evi <adevyskz31@gmail.com>
2025-10-29 20:14:15 +0800

    perulangan while

---
+++ Day52.java
@@ -0,0 +1,9 @@
+public class Day52 {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 3) {
+            System.out.println("Perulangan ke "+a);
+            a++;
+        }
+    }
+}
