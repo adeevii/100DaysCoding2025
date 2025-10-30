c05147a0d18bd0f366e58589eb03688f7a79b781
Ade Evi <adevyskz31@gmail.com>
2025-10-30 22:04:54 +0800

    keyword break

---
+++ Day53.java
@@ -0,0 +1,10 @@
+public class Day53 {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 5) {
+            if (a == 2) break;
+            System.out.println(a);
+            a++;
+        }
+    }
+}
