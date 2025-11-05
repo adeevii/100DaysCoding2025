14a26896c6f3558735d2d8f7b7bcc13b29e4b715
Ade Evi <adevyskz31@gmail.com>
2025-11-05 21:10:49 +0800

    Implement odd and even number printing

---
+++ Day59.java
@@ -0,0 +1,12 @@
+public class Day59 {
+    public static void main(String[] args) {
+        System.out.print("Ganjil: ");
+        for (int i = 1; i <= 5; i += 2) {
+            System.out.print(" "+i);
+        }
+        System.out.print("\nGenap: ");
+        for (int i = 2; i <= 5; i += 2) {
+            System.out.print(" "+i);
+        }
+    }
+}
