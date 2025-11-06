0f0426d85ad659430ceda52c2b887d4cf6f723a2
Ade Evi <adevyskz31@gmail.com>
2025-11-06 22:07:57 +0800

    Fix output formatting for odd and even numbers

--- Day60.java
+++ Day60.java
@@ -2,12 +2,12 @@ public class Day60 {
     public static void main(String[] args) {
         System.out.print("Angka ganjil: ");
         for (int i = 1; i <= 5; i += 2) {
-            System.out.print(i + " ");
+            System.out.print(" "+i);
         }
 
         System.out.print("\nAngka genap : ");
         for (int i = 2; i <= 5; i += 2) {
-            System.out.print(i + " ");
+            System.out.print(" "+i);
         }
     }
 }
