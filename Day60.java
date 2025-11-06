f671e04c289467d8d5dae305d7af95c1807e768e
Ade Evi <adevyskz31@gmail.com>
2025-11-06 22:01:46 +0800

    Update output to display odd and even numbers

--- Day60.java
+++ Day60.java
@@ -1,8 +1,13 @@
 public class Day60 {
     public static void main(String[] args) {
-        System.out.print("Bilangan kelipatan 3: ");
-        for (int i = 3; i <= 20; i += 3) {
-            System.out.print(" "+i);
+        System.out.print("Angka ganjil: ");
+        for (int i = 1; i <= 5; i += 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\nAngka genap : ");
+        for (int i = 2; i <= 5; i += 2) {
+            System.out.print(i + " ");
         }
     }
 }
