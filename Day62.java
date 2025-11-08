42e2da68103adfefd7376b5e359e30db3f0ba180
Ade Evi <adevyskz31@gmail.com>
2025-11-08 22:55:12 +0800

    Implement sum calculation from 1 to 5
    Calculate the sum of numbers from 1 to 5 and print the result.

---
+++ Day62.java
@@ -0,0 +1,10 @@
+public class Day62 {
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            total += i;
+        }
+        System.out.println("Jumlah bilangan 1-10: "+total);
+    }
+}
