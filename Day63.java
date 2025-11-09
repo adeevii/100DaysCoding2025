0bebb8e104dedfc16ede1a9435c8930548c589b5
Ade Evi <adevyskz31@gmail.com>
2025-11-09 20:15:58 +0800

    Fix multiplication logic in Day63.java
    Initialize total to 1 for correct multiplication.

---
+++ Day63.java
@@ -0,0 +1,10 @@
+public class Day63 {
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            total *= i;
+        }
+        System.out.println("Hasil perkalian : "+total);
+    }
+}
