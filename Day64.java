70398b1e3da2e9dfeb80eeb28b02c0d7e7797613
Ade Evi <adevyskz31@gmail.com>
2025-11-10 21:12:46 +0800

    Add Day64 class to calculate power of ten

---
+++ Day64.java
@@ -0,0 +1,10 @@
+public class Day64 {
+    public static void main(String[] args) {
+        int hasil = 1;
+        for (int i = 1; i <= 2; i++) {
+            hasil *= 10;
+        }
+        
+        System.out.println("Hasil 10 pangkat 2: "+hasil);
+    }
+}
