83f6beb4215d80e9c3015a14b63d04fae631edb9
Ade Evi <adevyskz31@gmail.com>
2025-11-13 21:34:13 +0800

    pola horizontal

---
+++ Day67.java
@@ -0,0 +1,10 @@
+public class Day67 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        for (int i = 0; i < a; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
