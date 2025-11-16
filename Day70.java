4c3af80d9fe65ac46c1d194c82f39fc4ddce00a5
Ade Evi <adevyskz31@gmail.com>
2025-11-16 18:46:33 +0800

    Pola persegi panjang

---
+++ Day70.java
@@ -0,0 +1,13 @@
+public class Day70 {
+    public static void main(String[] args) {
+        int panjang = 4;
+        int lebar = 8;
+
+        for (int i = 1; i <= panjang; i++) {
+            for (int j = 1; j <= lebar; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
