890eca9f0b00604a973c2df829fafc7b2414113f
Ade Evi <adevyskz31@gmail.com>
2025-11-26 20:07:39 +0800

    Array 1D

---
+++ Day80.java
@@ -0,0 +1,11 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int[] a = {2, 10, 8, 6, 4};
+
+        System.out.println("Isi Array 1D: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Elemen ke-"+ i + " = "+ a[i]);
+            
+        }
+    }
+}
