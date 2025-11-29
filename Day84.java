adbee450be94f029dd328dc0e557a19ffcef6344
Ade Evi <adevyskz31@gmail.com>
2025-11-29 20:38:36 +0800

    Menjumlahkan array 1D

---
+++ Day84.java
@@ -0,0 +1,12 @@
+public class Day84 {
+    public static void main(String[] args) {
+        int[] a = {5, 2, 4, 10};
+        int total = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            total += a[i];
+
+        }
+        System.out.println("Jumlah: "+total);
+    }
+}
