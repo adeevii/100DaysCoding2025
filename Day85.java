546f121dfb8a533c6b341f10af677a024f75f162
Ade Evi <adevyskz31@gmail.com>
2025-12-01 20:42:26 +0800

    Add files via upload
    memaximalkan elemen array

---
+++ Day85.java
@@ -0,0 +1,13 @@
+public class Day85 {
+    public static void main(String[] args) {
+        int[] a = {2, 4, 6, 8, 10};
+        int max = a[0];
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max){
+                max = a[i];
+            }
+        }
+        System.out.println("Angka maksimalnya adalah: "+max);
+    }
+}
