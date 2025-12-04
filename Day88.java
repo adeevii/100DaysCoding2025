5ba222dc3a5028a03320443d64619ea1ad5046b1
Ade Evi <adevyskz31@gmail.com>
2025-12-04 20:52:02 +0800

    Mengakses Elemen Array Menggunkan For Each

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Day88 {
+    public static void main(String[] args) {
+        System.out.println("Mengakses Elemen Array Menggunkan For Each");
+        String [] a = {"Halo","Hai","Hello"};
+
+        for (String teks : a){
+            System.out.println(teks);
+        }
+    }
+}
