7dd1dde6b0442f7625bab9d90d5101fb3da3df15
Ade Evi <adevyskz31@gmail.com>
2025-12-05 20:23:07 +0800

    Add files via upload
    method void

---
+++ Day89.java
@@ -0,0 +1,9 @@
+public class Day89 {
+        static void a(){
+            System.out.println("=====Program Day 89=====");
+    }
+
+    public static void main(String[] args) {
+        a();
+    }
+}
