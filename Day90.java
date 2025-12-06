10708dd25be52c7e4fc0cb8217dd0685ed928963
Ade Evi <adevyskz31@gmail.com>
2025-12-06 19:03:59 +0800

    Add files via upload
    method non-void

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class Day90 {
+    static int angka(){
+        return 20;
+    }
+
+    public static void main(String[] args) {
+        int a = angka();
+        System.out.println("Hasil nilai dari method: "+a);
+    }
+}
