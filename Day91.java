bf92aead03101cdfa9bd2600aa5d89a7c220f65c
Ade Evi <adevyskz31@gmail.com>
2025-12-07 20:50:54 +0800

    Add files via upload
    parameter void

---
+++ Day91.java
@@ -0,0 +1,9 @@
+public class Day91 {
+    static void nama(String a){
+        System.out.println("Haii "+a+", welcome yaa");
+    }
+
+    public static void main(String[] args) {
+        nama("Na Jaemin");
+    }
+}
