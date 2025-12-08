215819a523c050372ca74757c460e1a48227d3c5
Ade Evi <adevyskz31@gmail.com>
2025-12-08 19:53:38 +0800

    Add files via upload
    keyword return

---
+++ Day92.java
@@ -0,0 +1,10 @@
+public class Day92 {
+    static int a(int x, int y){
+        return x + y; //mengembalikan hasil penjumlahan
+    }
+
+    public static void main(String[] args) {
+        int hasil = a(10, 4); //memanggil method dan menangkap nilai method
+        System.out.println("Hasil: "+hasil);
+    }
+}
