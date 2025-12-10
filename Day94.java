5776a4cdf472fc38a40c4a69a61b919ead5d2c3c
Ade Evi <adevyskz31@gmail.com>
2025-12-10 22:20:59 +0800

    Add files via upload
    mencetak dengan method void

---
+++ Day94.java
@@ -0,0 +1,10 @@
+public class Day94 {
+    static void a(String b){
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        a("Halo, selamat datang");
+        a("Ini adee");
+        a("Salam kenal yaa");
+    }
+}
