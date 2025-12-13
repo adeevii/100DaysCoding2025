38f8c184a9746716890353e10b3245a43f94ca1d
Ade Evi <adevyskz31@gmail.com>
2025-12-13 22:40:05 +0800

    Add files via upload
    menghitung luas persegi panjang dengan method non-void dan parameternya

---
+++ Day97.java
@@ -0,0 +1,12 @@
+public class Day97 {
+    static int luas(int p, int l){
+        return p * l;
+    }
+
+    public static void main(String[] args) {
+        int pa = 10;
+        int le = 5;
+        int hasil =luas(pa, le);
+        System.out.println("Luas persegi panjangnya: "+hasil);
+    }
+}
